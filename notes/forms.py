a745b37f0e4fd56ff3efe4bfbeac0ef204fc0858
goodsujan <ethical.suzan@gmail.com>
2026-02-18 23:51:18 +0545

    Refactor NoteForm to use ModelForm and add validation for description length; update tests for note creation

--- notes/forms.py
+++ notes/forms.py
@@ -1,16 +1,52 @@
+# from django import forms
+# from .models import Note
+
+# class NoteForm(forms.Form):
+#     title = forms.CharField(
+#         max_length=100,
+#         error_messages={'required':'Title is required'}
+#     )
+#     description = forms.CharField(
+#         widget=forms.Textarea(),
+#         error_messages={'required':'Description is needed'}
+#     )
+#     def clean_description(self):
+#         des = self.cleaned_data.get('description','')
+#         if len(des) < 10:
+#             raise forms.ValidationError('Description must be at least 10 char long')
+#         return des
+
+
+#     def create(self, validated_data):
+#         return Note.objects.create(**validated_data)
+
 from django import forms
 from .models import Note
 
 
 class NoteForm(forms.Form):
-    title = forms.CharField(max_length=100,
-                            error_messages={
-                                'required': 'Title is required.'}
-                            )
-    description = forms.CharField(widget=forms.Textarea(),
-                                  error_messages={
-                                      'required': 'Please enter a description.'}
-                                  )
+    title = forms.CharField(
+        max_length=100,
+        error_messages={'required': 'Title is required'}
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(),
+        error_messages={'required': 'Description is needed'}
+    )
 
     def create(self, validated_data):
         return Note.objects.create(**validated_data)
+
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'description']
+
+    def clean_description(self):
+        des = self.cleaned_data.get('description', '')
+        if len(des) < 10:
+            raise forms.ValidationError(
+                'Description must be at least 10 char long'
+            )
+        return des
--- notes/tests.py
+++ notes/tests.py
@@ -1,7 +1,26 @@
 from django.test import TestCase
 from .utils import add
 
+from django.urls import reverse
+
 
 class AddTestCase(TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 7)
+        self.assertEqual(add(2, 3), 5)
+# Create your tests here.
+        self.assertEqual(add(2, 3), 5)
+
+
+class NotesTestCase(TestCase):
+    def test_notes_can_be_added(self):
+        res = self.client.post(
+            reverse('notes:add'),
+            {
+                'title': 'hello',
+                'description': 'notes from test'
+            }
+        )
+        self.assertEqual(res.status_code, 302)
+
+        reshome = self.client.get(reverse('notes:home'))
+        self.assertContains(reshome, "hello")
