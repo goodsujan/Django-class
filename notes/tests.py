e045bab1c96e4989be28b4149487ef8bed412d6a
goodsujan <ethical.suzan@gmail.com>
2026-02-19 09:30:30 +0545

    Add Django CI workflow, enhance tests, and update requirements

--- notes/tests.py
+++ notes/tests.py
@@ -1,26 +1,45 @@
 from django.test import TestCase
+from django.urls import reverse
 from .utils import add
 
-from django.urls import reverse
 
-
-class AddTestCase(TestCase):
+class AddFunctionTestCase(TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
-# Create your tests here.
-        self.assertEqual(add(2, 3), 5)
+
+
+class HomePageTestCase(TestCase):
+    def test_home_page_contains_homepage_text(self):
+        response = self.client.get(reverse('notes:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Homepage')
 
 
 class NotesTestCase(TestCase):
-    def test_notes_can_be_added(self):
-        res = self.client.post(
-            reverse('notes:add'),
-            {
-                'title': 'hello',
-                'description': 'notes from test'
-            }
-        )
-        self.assertEqual(res.status_code, 302)
+    def test_notes_can_be_created(self):
+        # Act
+        response = self.client.post(reverse('notes:add'), {
+            'title': 'Django Course1',
+            'description': 'Complete course with urls, templates, models, etc'
+        })
 
-        reshome = self.client.get(reverse('notes:home'))
-        self.assertContains(reshome, "hello")
+        # Assert
+        # Redirect after successful creation
+        self.assertEqual(response.status_code, 302)
+
+        # Follow redirect and check the note appears
+        response = self.client.get(response.url)
+        self.assertContains(response, 'Django Course')
+
+    def test_error_occurs_if_description_is_less_than_10_chars_long(self):
+        # Act
+        response = self.client.post(reverse('notes:add'), {
+            'title': 'Django Course',
+            'description': 'dj'
+        })
+
+        # Assert
+        # Should return to form with errors
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response, 'Description must be at least 10 characters long')
